6c953d8512af5c9a4d66f610d11f7ced71d657e0
piekarowa <pau.piekarska@gmail.com>
2022-02-01 12:56:00 +0100

    Completed Twice as old kata

---
+++ src/main/java/com/example/codewars/calculatebmi/CalculateBMI.java
@@ -0,0 +1,15 @@
+package com.example.codewars.calculatebmi;
+
+public class CalculateBMI {
+
+    private final double weight;
+    private final double height;
+
+    public CalculateBMI(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+
+
+}
