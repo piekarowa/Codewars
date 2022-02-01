ff3faf42c4be66bcf1894f500307e91231c17e92
piekarowa <pau.piekarska@gmail.com>
2022-02-01 12:56:48 +0100

    Completed CalculateBMI kata

--- src/main/java/com/example/codewars/calculatebmi/CalculateBMI.java
+++ src/main/java/com/example/codewars/calculatebmi/CalculateBMI.java
@@ -10,6 +10,19 @@ public class CalculateBMI {
         this.height = height;
     }
 
+    public String solution() {
+        double bmi = weight / (height * height);
+        if (bmi > 30) {
+            return "Obese";
+        } else if (bmi > 25.0 && bmi <= 30) {
+            return "Overweight";
+        } else if (bmi > 18.5 && bmi <= 25.0) {
+            return "Normal";
+        } else {
+            return "Underweight";
+        }
+    }
+
 
 
 }
