5dd0e7587fabc390a5518ffd14538989ca749f51
piekarowa <pau.piekarska@gmail.com>
2022-11-24 23:29:03 +0100

    Completed YesOrNo kata

---
+++ src/main/java/com/example/codewars/YesOrNo/YesOrNo.java
@@ -0,0 +1,18 @@
+package com.example.codewars.YesOrNo;
+
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+public class YesOrNo {
+
+
+        public static String boolToWorld(boolean b){
+
+        if(b){
+            return "Yes";
+        }else{
+            return "No";
+        }
+
+    }
+}
