4a2649af49f37324a568d5faa6d0402bb7b1636e
piekarowa <pau.piekarska@gmail.com>
2022-11-26 22:00:12 +0100

    Completed L1: SetAlarm kata

---
+++ src/main/java/com/example/codewars/SetAlarm/SetAlarm.java
@@ -0,0 +1,18 @@
+package com.example.codewars.SetAlarm;
+
+public class SetAlarm {
+
+
+    public static boolean setAlarm(boolean employed, boolean vacation) {
+
+        return employed && !vacation;
+
+    }
+}
+
+
+
+
+
+
+
