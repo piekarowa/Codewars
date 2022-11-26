07db68b62b8967082bd8da789f69e03ec81d48ed
piekarowa <pau.piekarska@gmail.com>
2022-11-26 21:36:27 +0100

    Completed Troll kata

---
+++ src/main/java/com/example/codewars/Troll/Troll.java
@@ -0,0 +1,22 @@
+package com.example.codewars.Troll;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Troll {
+
+    public static String disemvowel(String str){
+
+
+          return   str.replaceAll("[aeiou]", "");
+
+    }
+
+    public static void main(String[] args) {
+        String example = "The input string contains vowels";
+
+        System.out.println(disemvowel(example));
+
+    }
+
+}
