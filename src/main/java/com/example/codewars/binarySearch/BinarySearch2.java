90c07769f63493714248747302162a1e2258428c
piekarowa <pau.piekarska@gmail.com>
2023-04-03 12:47:21 +0200

    binarySearch

--- src/main/java/com/example/codewars/binarySearch/BinarySearch2.java
+++ src/main/java/com/example/codewars/binarySearch/BinarySearch2.java
@@ -4,6 +4,22 @@ public class BinarySearch2 {
 
     public static void main(String[] args) {
 
+        int[] ints = {1, 2, 5, 7, 9, 11, 13};
+
+        System.out.println(binarySearch2(ints, 15));
+
+
+    }
+
+    private static int binarySearch2(int[] numbers, int numberToFind){
+
+        int low = 0;
+
+        int high = numbers.length - 1;
+
+
+
+
     }
 
 }
