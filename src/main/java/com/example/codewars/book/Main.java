9a2f3f37ddc2bd0b19417c8508eab87074564eb2
piekarowa <pau.piekarska@gmail.com>
2023-04-20 12:16:09 +0200

    book algorythms

--- src/main/java/com/example/codewars/book/Main.java
+++ src/main/java/com/example/codewars/book/Main.java
@@ -1,7 +1,24 @@
 package com.example.codewars.book;
 
+import javax.swing.*;
+import java.security.Key;
+import java.util.Stack;
+
 public class Main {
 
 
+    public void lookForKey(Box mainBox) {
+        Stack<Box> pile = mainBox.makeAPileToLookThrough();
+        while (!pile.isEmpty()) {
+            Box box = pile.pop();
+            for (Item item : box.getItems()) {
+                if (item instanceof Box) {
+                    pile.push((Box) item);
+                } else if (item instanceof Key) {
+                    System.out.println("key found");
+                }
+            }
+        }
+    }
 
 }
