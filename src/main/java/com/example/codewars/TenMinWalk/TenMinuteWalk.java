15e5242b5da78ac7e1597cef35c5c0d8d72b4fb2
piekarowa <pau.piekarska@gmail.com>
2023-05-26 13:48:28 +0200

    switch

--- src/main/java/com/example/codewars/TenMinWalk/TenMinuteWalk.java
+++ src/main/java/com/example/codewars/TenMinWalk/TenMinuteWalk.java
@@ -1,4 +1,25 @@
 package com.example.codewars.TenMinWalk;
 
 public class TenMinuteWalk {
+
+    public static boolean isValid(char[] walk) {
+
+        if(walk.length != 10){
+            return false;
+        }
+
+        int countE = 0;
+        int countW = 0;
+        int countN = 0;
+        int countS = 0;
+
+        for(char direction : walk){
+            switch (direction){
+
+            }
+        }
+
+    }
+
+
 }
