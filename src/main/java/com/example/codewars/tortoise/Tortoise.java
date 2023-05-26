e048741ff9660121de5063a3f81a3773270d8030
piekarowa <pau.piekarska@gmail.com>
2023-05-26 14:34:28 +0200

    speed

--- src/main/java/com/example/codewars/tortoise/Tortoise.java
+++ src/main/java/com/example/codewars/tortoise/Tortoise.java
@@ -1,4 +1,29 @@
 package com.example.codewars.tortoise;
 
 public class Tortoise {
+
+    public static int[] race(int v1, int v2, int g) {
+
+        if(v1 >= v2){
+            return null;
+        }
+
+        int speedDifference = v2 - v1;
+        int hour = g / speedDifference;
+
+        int minute = hour * 3600 / 60;
+        int second = hour * 3600 % 60;
+
+        return new int[]{hour, minute, second};
+
+
+    }
+
 }
+
+//A starts 720 feet/h
+//B runs faster than A, when starts aA has 70 feet lead, runs 850 feet/h
+//how long it takes B to catch A
+//v1 = 720, v2 = 850, g = 70
+//B will catch A when they run same distance at the same time
+//v = s/t
