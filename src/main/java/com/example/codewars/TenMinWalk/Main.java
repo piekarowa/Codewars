eb48089018b654b2faa8da52110831dc834b6c05
piekarowa <pau.piekarska@gmail.com>
2023-05-26 13:30:24 +0200

    good job

--- src/main/java/com/example/codewars/TenMinWalk/Main.java
+++ src/main/java/com/example/codewars/TenMinWalk/Main.java
@@ -1,9 +1,15 @@
 package com.example.codewars.TenMinWalk;
 
+import static com.example.codewars.TenMinWalk.TenMinWalk.isValid;
+
 public class Main {
 
     public static void main(String[] args) {
 
+char[] walk = {'w'};
+
+boolean isValid = isValid(walk);
+        System.out.println(isValid);
 
 
     }
