1ab3de828ace5cc5002c564eff56c151b51faef3
piekarowa <pau.piekarska@gmail.com>
2023-05-26 13:54:18 +0200

    switch

--- src/main/java/com/example/codewars/TenMinWalk/Main.java
+++ src/main/java/com/example/codewars/TenMinWalk/Main.java
@@ -1,17 +1,20 @@
 package com.example.codewars.TenMinWalk;
 
 import static com.example.codewars.TenMinWalk.TenMinWalk.isValid;
+import static com.example.codewars.TenMinWalk.TenMinuteWalk.isValid2;
 
 public class Main {
 
     public static void main(String[] args) {
 
 char[] walk = {'e','e','e','e','e','w','w','w','w','w'};
+char[] walk2 = {'e','e','e','e','e','w','w','w','w','w'};
 
 boolean isValid = isValid(walk);
         System.out.println(isValid);
 
-
+        boolean isValid2 = isValid(walk2);
+        System.out.println(isValid2);
     }
 
 }
--- src/main/java/com/example/codewars/TenMinWalk/TenMinuteWalk.java
+++ src/main/java/com/example/codewars/TenMinWalk/TenMinuteWalk.java
@@ -2,9 +2,9 @@ package com.example.codewars.TenMinWalk;
 
 public class TenMinuteWalk {
 
-    public static boolean isValid(char[] walk) {
+    public static boolean isValid2(char[] walk2) {
 
-        if(walk.length != 10){
+        if(walk2.length != 10){
             return false;
         }
 
@@ -13,11 +13,26 @@ public class TenMinuteWalk {
         int countN = 0;
         int countS = 0;
 
-        for(char direction : walk){
+        for(char direction : walk2){
             switch (direction){
-
+                case 'e':
+                    countE++;
+                    break;
+                case 'w':
+                    countW++;
+                    break;
+                case 'n':
+                    countN++;
+                    break;
+                case 's':
+                    countS++;
+                    break;
+                default:
+                    return false;
             }
         }
+
+        return countN == countS && countE == countW;
 
     }
 
