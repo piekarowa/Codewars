965bc05bb8726486fd5138de516440f444412566
piekarowa <pau.piekarska@gmail.com>
2023-05-26 12:56:02 +0200

    TenMinWalkFunction

--- src/main/java/com/example/codewars/TenMinWalk/TenMinWalk.java
+++ src/main/java/com/example/codewars/TenMinWalk/TenMinWalk.java
@@ -2,6 +2,12 @@ package com.example.codewars.TenMinWalk;
 
 public class TenMinWalk {
 
-
-
+    public static boolean isValid(char[] walk) {
+        // Insert brilliant code here
+        return true;
+    }
 }
+
+
+
+
