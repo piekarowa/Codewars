94d7acbe54fd366b6cce3910232d90ed02c63dcb
piekarowa <pau.piekarska@gmail.com>
2023-05-26 13:27:41 +0200

    Main

--- src/main/java/com/example/codewars/TenMinWalk/TenMinWalk.java
+++ src/main/java/com/example/codewars/TenMinWalk/TenMinWalk.java
@@ -1,12 +1,40 @@
 package com.example.codewars.TenMinWalk;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class TenMinWalk {
 
     public static boolean isValid(char[] walk) {
-        // Insert brilliant code here
-        return true;
+
+        String[] stringArray = new String[walk.length];
+        for(int i = 0; i < walk.length; i++){
+            stringArray[i] = String.valueOf(walk[i]);
+        }
+
+        List<String> list = Arrays.asList(stringArray);
+
+        int countN = Collections.frequency(list, "n");
+        int countS = Collections.frequency(list, "s");
+        int countE = Collections.frequency(list, "e");
+        int countW = Collections.frequency(list, "w");
+
+        return list.contains("e") && countE == countW &&
+                list.contains("n") && countN == countS &&
+                list.size() == 10;
+
+
     }
 }
+
+//walk takes 10 minutes
+//one movement takes 1 minute
+//you have to end in start point
+//'n', 's', 'w', 'e'
+//the same times e and w
+//the same time n and s
 
 
 
