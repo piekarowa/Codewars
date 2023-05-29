6aa4cf5f5bc90b355045182cce3142e6ae3e035e
piekarowa <pau.piekarska@gmail.com>
2023-05-29 13:41:35 +0200

    sortTheOdd

---
+++ src/main/java/com/example/codewars/sortTheOdd/Main.java
@@ -0,0 +1,9 @@
+package com.example.codewars.sortTheOdd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
--- src/main/java/com/example/codewars/sortTheOdd/SortTheOdd.java
+++ src/main/java/com/example/codewars/sortTheOdd/SortTheOdd.java
@@ -1,9 +1,42 @@
 package com.example.codewars.sortTheOdd;
 
+import java.util.Arrays;
+
 public class SortTheOdd {
 
+    public static boolean isEven(int num){
+        return num % 2 == 0;
+    }
+
+
     public static int[] sortArray(int[] array) {
+
+        int index = 0;
+        int[] sortedArray = new int[array.length];
+
+
+        for(int i = 0; i < array.length; i++){
+
+            int number = Integer.parseInt(String.valueOf(array[i]));
+
+            if(isEven(array[i])){
+                sortedArray[i] = array[i];
+            }else{
+                sortedArray[index++] = array[i];
+            }
+
+        }
+
+        Arrays.sort(sortedArray, 0 , index);
+
+        index = 0;
+        for(int i = 0; i < array.length; i++){
+            if(!isEven(array[i])){
+                array[i] = sortedArray[index++];
+            }
+        }
         return array;
+
     }
 
 }
