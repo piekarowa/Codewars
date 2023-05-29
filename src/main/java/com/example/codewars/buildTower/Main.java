bd4f9448a3ba1f748342f963df3ff172683d618e
piekarowa <pau.piekarska@gmail.com>
2023-05-29 12:35:26 +0200

    tower

--- src/main/java/com/example/codewars/buildTower/Main.java
+++ src/main/java/com/example/codewars/buildTower/Main.java
@@ -1,4 +1,21 @@
 package com.example.codewars.buildTower;
 
+import static com.example.codewars.buildTower.TowerBuilder.towerBuilder;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        int nFloors = 4;
+
+        String[] tree = towerBuilder(nFloors);
+
+        for(String line : tree){
+            System.out.println(line);
+        }
+
+
+
+    }
+
 }
